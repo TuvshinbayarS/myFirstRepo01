a2f377570dc1ecbd8ea77976570b98fbd31d1302
tuvshinbayarS <tuvshinbayar.surenkhorloo@tekschool.us>
2023-02-15 21:52:34 -0500

    Unit test class update added

--- src/gitTest/unitTest.java
+++ src/gitTest/unitTest.java
@@ -5,6 +5,14 @@ public class unitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		int a=10;
+		int b=15;
+		int c=a+b;
+		
+		System.out.println(c);		
+		
+		
 	}
 
 }
